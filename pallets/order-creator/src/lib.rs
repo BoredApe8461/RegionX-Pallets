72528bf8500fc130758eae485de2434bb6dd3587
BoredApe8461 <letesinkelegesse@gmail.com>
2024-05-31 17:58:11 +0200

    progress

--- pallets/order-creator/src/lib.rs
+++ pallets/order-creator/src/lib.rs
@@ -5,207 +5,107 @@
 use frame_support::traits::Currency;
 use frame_system::WeightInfo;
 pub use pallet::*;
-use xcm_executor::traits::ConvertLocation;
-
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
 
 mod types;
 pub use crate::types::*;
 
-pub type BalanceOf<T> =
-	<<T as crate::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+pub type BalanceOf<T> = <<T as crate::Config>::RelaychainCurrency as Currency<
+    <T as frame_system::Config>::AccountId,
+>>::Balance;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use super::*;
-	use frame_support::{
-		pallet_prelude::*,
-		sp_runtime::Saturating,
-		traits::{fungible::Transfer, Get, ReservableCurrency},
-	};
-	use frame_system::pallet_prelude::*;
+    use super::*;
+    use frame_support::{
+        pallet_prelude::*,
+        traits::{fungible::Mutate, Get},
+    };
+    use frame_system::pallet_prelude::*;
+    use sp_runtime::traits::BlockNumberProvider;
 
-	/// The module configuration trait.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// The overarching event type.
-		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+    /// The module configuration trait.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-		/// The relay chain currency used for coretime procurement.
-		type RelaychainCurrency: Transfer<Self::AccountId>;
+        /// The relay chain currency used for coretime procurement.
+        type RelaychainCurrency: Mutate<Self::AccountId>;
 
-        // TODO: this is not for this pallet, this is for the renewal financing. just noting this here so I don't forget the idea.
-        //
-        // The expense cap will ensure that the RegionX chain can't spam new sale notifications.
-        type ExpenseCap;
+        /// A means of getting the current relay chain block.
+        ///
+        /// This is used for determining the current timeslice.
+        type RCBlockNumberProvider: BlockNumberProvider;
 
-		/// Weight Info
-		type WeightInfo: WeightInfo;
-	}
+        /// The admin origin for managing the order creation.
+        type AdminOrigin: EnsureOrigin<Self::RuntimeOrigin>;
 
-	#[pallet::pallet]
-	pub struct Pallet<T>(_);
+        /// Weight Info
+        type WeightInfo: WeightInfo;
+    }
 
-	/// Created orders.
-	#[pallet::storage]
-	#[pallet::getter(fn coretime_requirement)]
-	pub type CoretimeRequirement<T: Config> = StorageValue<_, Requirements, OptionQuery>;
+    #[pallet::pallet]
+    pub struct Pallet<T>(_);
 
-    
-	/// Next order id
-	#[pallet::storage]
-	#[pallet::getter(fn next_order_id)]
-	pub type NextOrderId<T> = StorageValue<_, OrderId, ValueQuery>;
+    /// The coretime requirements for the parachain.
+    ///
+    /// Orders will be made based on this.
+    #[pallet::storage]
+    #[pallet::getter(fn coretime_requirements)]
+    pub type CoretimeRequirements<T: Config> = StorageValue<_, Requirements, OptionQuery>;
 
-	/// Crowdfunding contributions.
-	#[pallet::storage]
-	#[pallet::getter(fn contributions)]
-	pub type Contributions<T: Config> = StorageDoubleMap<
-		_,
-		Blake2_128Concat,
-		OrderId, // the order.
-		Blake2_128Concat,
-		T::AccountId, // the account which contributed to the order.
-		BalanceOf<T>, // the amount they contributed.
-		ValueQuery,
-	>;
+    /// The current configuration of the Coretime chain.
+    ///
+    /// Can be modified by the `AdminOrigin`.
+    #[pallet::storage]
+    #[pallet::getter(fn configuration)]
+    pub type Configuration<T: Config> = StorageValue<_, ConfigRecordOf<T>, OptionQuery>;
 
-	/// The total amount that was contributed to an order.
-	///
-	/// The sum of contributions for a specific order from the Contributions map should be equal to
-	/// the total contribution stored here.
-	#[pallet::storage]
-	#[pallet::getter(fn total_contributions)]
-	pub type TotalContributions<T: Config> =
-		StorageMap<_, Blake2_128Concat, OrderId, BalanceOf<T>, ValueQuery>;
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {}
 
-	#[pallet::event]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
+    #[pallet::error]
+    #[derive(PartialEq)]
+    pub enum Error<T> {}
 
-	#[pallet::error]
-	#[derive(PartialEq)]
-	pub enum Error<T> {
-		/// Invalid order id
-		InvalidOrderId,
-		/// The caller is not the creator of the given order
-		NotAllowed,
-		/// The contribution amount is too small
-		InvalidAmount,
-		/// The given order is not cancelled
-		OrderNotCancelled,
-		/// The contributed amount equals to zero.
-		NoContribution,
-	}
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Set the configuration of the Coretime chain.
+        ///
+        /// - `origin`: Must be Root or pass `AdminOrigin`.
+        /// - `config`: The configuration the Coretime chain.
+        #[pallet::call_index(0)]
+        #[pallet::weight(10_000)] // TODO
+        pub fn set_configuration(
+            origin: OriginFor<T>,
+            config: ConfigRecordOf<T>,
+        ) -> DispatchResult {
+            T::AdminOrigin::ensure_origin_or_root(origin)?;
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Extrinsic for creating an order.
-		///
-		/// ## Arguments:
-		/// - `para_id`: The para id to which Coretime will be allocated.
-		/// - `requirements`: Region requirements of the order.
-		#[pallet::call_index(0)]
-		#[pallet::weight(10_000)] // TODO
-		pub fn create_order(
-			origin: OriginFor<T>,
-			para_id: ParaId,
-			requirements: Requirements,
-		) -> DispatchResult {
-			let who = ensure_signed(origin)?;
+            Configuration::<T>::put(config);
+            // TODO: event
 
-			T::OrderCreationFeeHandler::handle(&who, T::OrderCreationCost::get())?;
+            Ok(())
+        }
 
-			let order_id = NextOrderId::<T>::get();
-			Orders::<T>::insert(order_id, Order { creator: who, para_id, requirements });
-			NextOrderId::<T>::put(order_id + 1);
+        /// Extrinsic for creating an order.
+        ///
+        /// ## Arguments:
+        /// - `requirements`: The coretime requirements of the parachain. If set to `None` the pallet will stop with the order creation.
+        #[pallet::call_index(1)]
+        #[pallet::weight(10_000)] // TODO
+        pub fn set_coretime_requirements(
+            origin: OriginFor<T>,
+            requirements: Option<Requirements>,
+        ) -> DispatchResult {
+            T::AdminOrigin::ensure_origin_or_root(origin)?;
 
-			Self::deposit_event(Event::OrderCreated { order_id });
-			Ok(())
-		}
+            CoretimeRequirements::<T>::set(requirements);
 
-		/// Extrinsic for cancelling an order.
-		///
-		/// ## Arguments:
-		/// - `para_id`: The para id to which Coretime will be allocated.
-		/// - `requirements`: Region requirements of the order.
-		#[pallet::call_index(1)]
-		#[pallet::weight(10_000)] // TODO
-		pub fn cancel_order(origin: OriginFor<T>, order_id: OrderId) -> DispatchResult {
-			let who = ensure_signed(origin)?;
+            // TODO: event
 
-			let order = Orders::<T>::get(order_id).ok_or(Error::<T>::InvalidOrderId)?;
-			ensure!(order.creator == who, Error::<T>::NotAllowed);
-
-			Orders::<T>::remove(order_id);
-
-			Self::deposit_event(Event::OrderRemoved { order_id });
-			Ok(())
-		}
-
-		/// Extrinsic for contributing to an order.
-		///
-		/// ## Arguments:
-		/// - `order_id`: The order to which the caller wants to contribute.
-		/// - `amount`: The amount of tokens the user wants to contribute.
-		#[pallet::call_index(2)]
-		#[pallet::weight(10_000)] // TODO
-		pub fn contribute(
-			origin: OriginFor<T>,
-			order_id: OrderId,
-			amount: BalanceOf<T>,
-		) -> DispatchResult {
-			let who = ensure_signed(origin)?;
-
-			ensure!(Orders::<T>::get(order_id).is_some(), Error::<T>::InvalidOrderId);
-			ensure!(amount >= T::MinimumContribution::get(), Error::<T>::InvalidAmount);
-			T::Currency::reserve(&who, amount)?;
-
-			let mut contribution: BalanceOf<T> = Contributions::<T>::get(order_id, who.clone());
-
-			contribution = contribution.saturating_add(amount);
-			Contributions::<T>::insert(order_id, who.clone(), contribution);
-
-			let mut total_contributions = TotalContributions::<T>::get(order_id);
-
-			total_contributions = total_contributions.saturating_add(amount);
-			TotalContributions::<T>::insert(order_id, total_contributions);
-
-			Self::deposit_event(Event::Contributed { order_id, who, amount });
-
-			Ok(())
-		}
-
-		/// Extrinsic for removing contributions from a cancelled order.
-		///
-		/// ## Arguments:
-		/// - `order_id`: The cancelled order from which the user wants to claim back their
-		///   contribution.
-		#[pallet::call_index(3)]
-		#[pallet::weight(10_000)] // TODO
-		pub fn remove_contribution(origin: OriginFor<T>, order_id: OrderId) -> DispatchResult {
-			let who = ensure_signed(origin)?;
-
-			ensure!(Orders::<T>::get(order_id).is_none(), Error::<T>::OrderNotCancelled);
-
-			let amount: BalanceOf<T> = Contributions::<T>::get(order_id, who.clone());
-			ensure!(amount != Default::default(), Error::<T>::NoContribution);
-
-			T::Currency::unreserve(&who, amount);
-			Contributions::<T>::remove(order_id, who.clone());
-
-			let mut total_contributions = TotalContributions::<T>::get(order_id);
-
-			total_contributions = total_contributions.saturating_sub(amount);
-			TotalContributions::<T>::insert(order_id, total_contributions);
-
-			Self::deposit_event(Event::ContributionRemoved { who, order_id, amount });
-
-			Ok(())
-		}
-	}
+            Ok(())
+        }
+    }
 }
--- pallets/order-creator/src/types.rs
+++ pallets/order-creator/src/types.rs
@@ -1,12 +1,24 @@
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_system::pallet_prelude::BlockNumberFor;
+use pallet_broker::{ConfigRecord, PartsOf57600, Timeslice};
+use scale_info::TypeInfo;
+use sp_runtime::traits::BlockNumberProvider;
+
+/// Relay chain block number.
+pub type RCBlockNumberOf<T> =
+    <<T as crate::Config>::RCBlockNumberProvider as BlockNumberProvider>::BlockNumber;
+
+pub type ConfigRecordOf<T> = ConfigRecord<BlockNumberFor<T>, RCBlockNumberOf<T>>;
+
 /// The coretime region requirements for the parachain.
 ///
 /// This will describe the requirements in the coretime order.
 #[derive(Encode, Decode, Debug, Clone, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
 pub struct Requirements {
-	/// The timeslice at which the Region begins.
-	pub begin: Timeslice,
-	/// The timeslice at which the Region ends.
-	pub end: Timeslice,
-	/// The minimum fraction of the core that the region should occupy.
-	pub core_occupancy: PartsOf57600,
+    /// The timeslice at which the Region begins.
+    pub begin: Timeslice,
+    /// The timeslice at which the Region ends.
+    pub end: Timeslice,
+    /// The minimum fraction of the core that the region should occupy.
+    pub core_occupancy: PartsOf57600,
 }
