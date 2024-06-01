408f714598d24c895d3d43bf13f87c0c3ad226b2
BoredApe8461 <letesinkelegesse@gmail.com>
2024-06-01 10:24:33 +0200

    fixes

--- pallets/order-creator/src/lib.rs
+++ pallets/order-creator/src/lib.rs
@@ -67,22 +67,27 @@ pub mod pallet {
 	#[pallet::getter(fn configuration)]
 	pub type Configuration<T: Config> = StorageValue<_, ConfigRecordOf<T>, OptionQuery>;
 
-	/// A reference sale start, used for determining the current sale phase.
+	/// The start of the latest bulk period.
 	///
-	/// This can be set to the start of any sale as long as the sale duration hasn't changed in the
-	/// meantime.
+	/// When setting up the pallet, this should be set to the start of the latest bulk period by the
+	/// `AdminOrigin`.
 	///
-	/// Generally, it is best practice to set this to the start of the latest sale when initializing
-	/// the state of this pallet.
+	/// After that, the pallet will keep this up to date by itself.
 	///
-	/// *WARNING*: If the sale duration changes after this is set, it should be updated to ensure we
-	/// don't create redundant orders or miss order creation.
+	/// *WARNING*: If the sale duration changes on the Coretime chain, the `AdminOrigin` should
+	/// update the `Configuration` to match the new config on the Coretime chain. If not updated, we
+	/// run the risk of creating redundant orders or missing an order creation.
 	///
 	/// Ideally, we would read the current sale state directly from the Coretime chain. However,
 	/// that would require using something like ISMP and a relay infrastructure.
 	#[pallet::storage]
-	#[pallet::getter(fn reference_sale_start)]
-	pub type ReferenceSaleStart<T: Config> = StorageValue<_, ConfigRecordOf<T>, OptionQuery>;
+	#[pallet::getter(fn bulk_period_start)]
+	pub type BulkPeriodStart<T: Config> = StorageValue<_, Timeslice, OptionQuery>;
+
+	/// The timeslice when the last order was made.
+	#[pallet::storage]
+	#[pallet::getter(fn last_order)]
+	pub type LastOrder<T: Config> = StorageValue<_, Timeslice, ValueQuery>;
 
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -90,11 +95,19 @@ pub mod pallet {
 
 	#[pallet::error]
 	#[derive(PartialEq)]
-	pub enum Error<T> {}
+	pub enum Error<T> {
+		/// The `AdminOrigin` provided an invalid bulk period start.
+		InvalidBulkPeriodStart,
+	}
 
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
 		fn on_initialize(_now: BlockNumberFor<T>) -> Weight {
+			let last_order = LastOrder::<T>::get();
+			let config = Configuration::<T>::get();
+
+			//let current_sale_start = ReferenceSaleStart::<T>::get().saturating_add(config.)
+
 			Weight::zero()
 		}
 	}
@@ -119,12 +132,36 @@ pub mod pallet {
 			Ok(())
 		}
 
+		/// Set the start of the latest bulk period.
+		///
+		/// - `origin`: Must be Root or pass `AdminOrigin`.
+		/// - `bulk_period_start`: The start of the latest bulk period.
+		#[pallet::call_index(1)]
+		#[pallet::weight(10_000)] // TODO
+		pub fn set_bulk_period_start(
+			origin: OriginFor<T>,
+			bulk_period_start: Timeslice,
+		) -> DispatchResult {
+			T::AdminOrigin::ensure_origin_or_root(origin)?;
+
+			// Sanity check:
+			ensure!(
+				bulk_period_start <= Self::current_timeslice(),
+				Error::<T>::InvalidBulkPeriodStart
+			);
+			BulkPeriodStart::<T>::put(bulk_period_start);
+
+			// TODO: event
+
+			Ok(())
+		}
+
 		/// Extrinsic for creating an order.
 		///
 		/// ## Arguments:
 		/// - `requirements`: The coretime requirements of the parachain. If set to `None` the
 		///   pallet will stop with the order creation.
-		#[pallet::call_index(1)]
+		#[pallet::call_index(2)]
 		#[pallet::weight(10_000)] // TODO
 		pub fn set_coretime_requirements(
 			origin: OriginFor<T>,
