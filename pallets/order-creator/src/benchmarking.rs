986b67d54805df2bd628017f16a329e1e1db26d1
BoredApe8461 <letesinkelegesse@gmail.com>
2024-06-26 07:18:06 +0000

    benchmarking

--- pallets/order-creator/src/benchmarking.rs
+++ pallets/order-creator/src/benchmarking.rs
@@ -22,34 +22,66 @@ use super::*;
 use frame_benchmarking::v2::*;
 use frame_support::traits::Get;
 use frame_system::RawOrigin;
-
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
 }
+use frame_system::pallet_prelude::BlockNumberFor;
+use pallet_broker::ConfigRecord;
 
 #[benchmarks]
 mod benchmarks {
 	use super::*;
+	use frame_support::traits::EnsureOrigin;
+	use sp_runtime::Perbill;
 
 	#[benchmark]
 	fn set_configuration() -> Result<(), BenchmarkError> {
-		let configuration = ConfigRecord {
-			advance_notice: 10,
-			interlude_length: 7_200,
-			leadin_length: 21_600,
-			region_length: 1_260,
+		let origin =
+			T::AdminOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
+
+		let configuration = ConfigRecord::<BlockNumberFor<T>, RCBlockNumberOf<T>> {
+			advance_notice: 10u32.into(),
+			interlude_length: 7_200u32.into(),
+			leadin_length: 21_600u32.into(),
+			region_length: 1_260u32.into(),
 			ideal_bulk_proportion: Perbill::from_percent(40),
 			limit_cores_offered: None,
 			renewal_bump: Perbill::from_percent(40),
-			contribution_timeout: 1_260,
+			contribution_timeout: 1_260u32.into(),
 		};
 
 		#[extrinsic_call]
-		_(RuntimeOrigin::root(), configuration.clone());
+		_(origin as T::RuntimeOrigin, configuration.clone());
 
 		assert_last_event::<T>(Event::ConfigurationSet { configuration }.into());
 		Ok(())
 	}
 
-	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(vec![]), crate::mock::Test);
+	#[benchmark]
+	fn schedule_next_order() -> Result<(), BenchmarkError> {
+		let origin =
+			T::AdminOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
+		let next_order = 100u32.into();
+
+		#[extrinsic_call]
+		_(origin as T::RuntimeOrigin, next_order);
+
+		assert_last_event::<T>(Event::NextOrderScheduled { next_order }.into());
+		Ok(())
+	}
+
+	#[benchmark]
+	fn set_coretime_requirements() -> Result<(), BenchmarkError> {
+		let origin =
+			T::AdminOrigin::try_successful_origin().map_err(|_| BenchmarkError::Weightless)?;
+		let requirements = Some(GenericRequirements { core_occupancy: 28800u16.into() });
+
+		#[extrinsic_call]
+		_(origin as T::RuntimeOrigin, requirements.clone());
+
+		assert_last_event::<T>(Event::CoretimeRequirementSet { requirements }.into());
+		Ok(())
+	}
+
+	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
 }
